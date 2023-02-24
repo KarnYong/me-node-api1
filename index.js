a7a7a6caeb5c72a9bdfcda54e204b7b2caff7396
Karn Yongsiriwit <karnyong@gmail.com>
2023-02-24 23:23:41 +0700

    use mysql pool

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
+// create a MySQL connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'test'
+});
 
 // Create a new Express app
 const app = express();
@@ -14,37 +22,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define your API endpoints
 app.get('/users', (req, res) => {
-  // Query the database to get all users
-  connection.query('SELECT * FROM users', (error, results, fields) => {
-    if (error) throw error;
-    res.send(results);
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.json(results);
+    }
   });
 });
 
-app.post('/users', (req, res) => {
-  // Insert a new user into the database
-  const { name, email } = req.body;
-  connection.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (error, results, fields) => {
-    if (error) throw error;
-    res.send(results);
-  });
-});
-
-// Set up a MySQL connection
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-
-// Connect to the MySQL server
-connection.connect((error) => {
-  if (error) throw error;
-  console.log('Connected to the MySQL server.');
-});
-
-// Start the Express server
-app.listen(5000, () => {
-  console.log('Server started on port 5000.');
+// start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
